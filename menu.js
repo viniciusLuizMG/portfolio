f3f621a1269f9e794f34cd9854476950cce22dbe
Vinicius_Luiz <166052594+viniciusLuizMG@users.noreply.github.com>
2024-07-02 15:28:10 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,17 @@
+
+// menu.js
+let btnMenu = document.getElementById('btn-menu'); // Corrigido para '#btn-menu'
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+});
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+});
